4bf6138c981b39c6821e058edd355b48133abda5
Radu Soltan <radusoltan@gmail.com>
2024-06-09 18:24:35 +0200

    Old Deschide // updated: - sitemap problem, rty; - next config 2; - catgory route;

--- app/[locale]/api/categories/route.js
+++ app/[locale]/api/categories/route.js
@@ -2,6 +2,6 @@ import axios from "@/lib/axios";
 import {NextResponse} from "next/server";
 
 export async function GET(request, {params: {locale}}) {
-  const categories = await axios.get(`api/public/homepage/categories?locale=${locale}`)
+  const categories = await axios.get(`/api/public/homepage/categories?locale=${locale}`)
   return NextResponse.json(categories.data)
 }
